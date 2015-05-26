92eb007bc196748c501e6407f3d8df55a8eb8530
Princess Sampson <ps@kingonomics.com>
2015-05-26 19:01:40 -0400

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log('This is cool!');
+function pluck(obj, key) {
+  
+});
+}
+
+function filter(list, callback) {
+
+var results = [];
+
+return results;
+
+}
+
+function find(list, predicate) {
+
+}
