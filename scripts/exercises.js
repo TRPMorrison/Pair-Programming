4fe739616c7bec4a68208646813f004a6c24c096
Trini Morrison <TRPMorrison@gmail.com>
2015-05-26 18:56:50 -0400

    ANSWERS TO SOME OF THEM

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -9,6 +9,52 @@
 function forEach(list, callback) { /* Do stuff */ }
 
 
+var Princess = [{
+    mom: "Shanterria", dad: "Rodney I", brother: "Rodney II", brother: "Rodney III", brother: "Rodney IV", brother: "Rodney V", sister: "Sofia"
+}]
+
+var Trini = [{ mom: "Marilyn", dad: "Howard",
+brother: "Disraeli",
+    sister: "Maria",
+    brother: "Talleyrand"
+}]
+
+
+function forEach(list, callback) {
+
+    for (var i = 0; i < list.length; i++) {
+
+        callback(list[i]);
+    }
+
+
+ };
+
+forEach(Princess, function(title) {
+
+    console.log('My mom\'s name is ' + title.mom);
+
+});
+
+forEach(Trini, function(title) {
+
+    console.log('My dad\'s name is ' + title.dad);
+
+});
+
+
+Princess.forEach(function (title) {
+console.log(Princess.mom);
+});
+
+
+Trini.forEach(function (title) {
+console.log(Trini.dad);
+});
+
+
+
+
 // 2. Write your own map, reduce, and filter functions that use your custom forEach to do their work.
 
 
@@ -27,6 +73,12 @@ function pluck(list, propertyName) { /* Do stuff*/ }
 var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
 pluck(stooges, 'name');
 // => ["moe", "larry", "curly"]
+
+
+// var who = stooges.map(function(stooge) {
+//     return stooge.name
+// });
+
 
 
 // 4. Write a function called find that looks through each value in the list, returning the first one that passes a truth test (predicate), or undefined if no value passes the test. The function returns as soon as it finds an acceptable element, and doesn't traverse the entire list.
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,53 @@
-var Princess = [{
-    mom: "Shanterria", dad: "Rodney I", brother: "Rodney II", brother: "Rodney III", brother: "Rodney IV", brother: "Rodney V", sister: "Sofia"
-}]
+// var Princess = [{
+//     mom: "Shanterria", dad: "Rodney I", brother: "Rodney II", brother: "Rodney III", brother: "Rodney IV", brother: "Rodney V", sister: "Sofia"
+// }]
 
-var Trini = [{ mom: "Marilyn", dad: "Howard",
-brother: "Disraeli",
-    sister: "Maria",
-    brother: "Talleyrand"
-}]
+// var Trini = [{ mom: "Marilyn", dad: "Howard",
+// brother: "Disraeli",
+//     sister: "Maria",
+//     brother: "Talleyrand"
+// }]
+
+var PrincessFamily = [
+    "Shanterria", "Rodney I", "Rodney II", "Rodney III", "Rodney IV", "Rodney V", "Sofia"
+];
+
+function forEach(list, callback) {
+
+    for (var i = 0; i < list.length; i++) {
+
+        callback(list[i]);
+    }
+
+ };
+
+// forEach(Princess, function(title) {
+
+//     console.log('My mom\'s name is ' + title.mom);
+
+// });
+
+// forEach(Trini, function(title) {
+
+//     console.log('My dad\'s name is ' + title.dad);
+
+// });
 
 
-// var Trini = [dad: 'Howard', mom: 'Marilyn', brother: 'Disraeli', sister: 'Maria', brother: 'Talleyrand'];
+// Princess.forEach(function (title) {
+// console.log(Princess.mom);
+// });
+
+
+// Trini.forEach(function (title) {
+// console.log(Trini.dad);
+// });
+
+forEach(Princess, function(title) {
+
+    ('My mom\'s name is ' + title.mom);
+
+});
 
 function forEach(list, callback) {
 
@@ -21,20 +59,20 @@ function forEach(list, callback) {
 
  };
 
-forEach(Princess, function(title) {
+ var PrincessFamily = [
+    "Shanterria", "Rodney I", "Rodney II", "Rodney III", "Rodney IV", "Rodney V", "Sofia"
+];
 
-console.log('My mom\'s name is ' + title.mom);
+PrincessFamily.forEach(function(name) { console.log(name.length);
+});
 
+PrincessFamily.filter(function(names) {
+return names;
 });
 
 
-
-
-
-
-
-
-
-
-
+//  3.
+// var who = stooges.map(function(stooge) {
+//     return stooge.name
+// });
 
