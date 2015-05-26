571c9a3ce985bceb601433c18e188a43a59da8e6
Trini Morrison <TRPMorrison@gmail.com>
2015-05-26 14:21:45 -0400

    JS HINT INSTALLED

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-console.log('This is cool!');
+var Princess = [dad: 'Rodney', mom: 'Shanterria', sister: 'Sofia', brother: 'Rodney II', 'Rodney III', 'Rodney IV', 'Rodney V'];
+
+var Trini = [dad: 'Howard', mom: 'Marilyn', brother: 'Disraeli', sister: 'Maria', brother: 'Talleyrand'];
+
+function forEach(list, callback) {
+
+
+
+ }
