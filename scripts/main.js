b2e4cfd8bfd935404fae98fa232cd4e20cf1926d
Trini Morrison <TRPMorrison@gmail.com>
2015-05-26 16:02:33 -0400

    NUMBER ONE ADDED

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,40 @@
-var Princess = [dad: 'Rodney', mom: 'Shanterria', sister: 'Sofia', brother: 'Rodney II', 'Rodney III', 'Rodney IV', 'Rodney V'];
+var Princess = [{
+    mom: "Shanterria", dad: "Rodney I", brother: "Rodney II", brother: "Rodney III", brother: "Rodney IV", brother: "Rodney V", sister: "Sofia"
+}]
 
-var Trini = [dad: 'Howard', mom: 'Marilyn', brother: 'Disraeli', sister: 'Maria', brother: 'Talleyrand'];
+var Trini = [{ mom: "Marilyn", dad: "Howard",
+brother: "Disraeli",
+    sister: "Maria",
+    brother: "Talleyrand"
+}]
+
+
+// var Trini = [dad: 'Howard', mom: 'Marilyn', brother: 'Disraeli', sister: 'Maria', brother: 'Talleyrand'];
 
 function forEach(list, callback) {
 
+    for (var i = 0; i < list.length; i++) {
+
+        callback(list[i]);
+    }
 
 
- }
+ };
+
+forEach(Princess, function(title) {
+
+console.log('My mom\'s name is ' + title.mom);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
